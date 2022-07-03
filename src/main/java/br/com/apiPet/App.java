fe99330456028fc7c803903c677a21e80a8b61c8
bleom <bleomar29@gmail.com>
2022-07-03 18:01:25 -0400

    first commit

---
+++ src/main/java/br/com/apiPet/App.java
@@ -0,0 +1,13 @@
+package br.com.apiPet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
